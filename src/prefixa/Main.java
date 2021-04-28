07b31b392ebb3b84c15c126a72d9c083a456d4cd
ednilsoncs <ednilsonsantos43@gmail.com>
2021-04-28 00:28:38 -0300

    feat: adicionado arquivo principal

---
+++ src/prefixa/Main.java
@@ -0,0 +1,29 @@
+package prefixa;
+import prefixa.lexer.*;
+import prefixa.node.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			String arquivo = "teste/teste.calc";
+
+			Lexer lexer =
+					new Lexer(
+							new PushbackReader(  
+									new FileReader(arquivo), 1024)); 
+			Token token;
+			while(!((token = lexer.next()) instanceof EOF)) {
+				System.out.print(token.getClass());
+				System.out.println(" ( "+token.toString()+")");
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
